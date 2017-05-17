57bba284fab401a486dfeb77e1ab5093dc522ce7
Michaël Zasso <targos@protonmail.com>
2017-05-17 15:02:22 +0200

    clear record when a new one is being fetched

--- src/reducers/record.js
+++ src/reducers/record.js
@@ -1,5 +1,8 @@
-import {handleAction} from 'redux-actions';
+import {handleActions} from 'redux-actions';
 
 import {RECORD_FETCH} from '../actions/types';
 
-export default handleAction(`${RECORD_FETCH}_FULFILLED`, (state, action) => action.payload, null);
+export default handleActions({
+    [`${RECORD_FETCH}_PENDING`]: () => null,
+    [`${RECORD_FETCH}_FULFILLED`]: (state, action) => action.payload
+}, null);
